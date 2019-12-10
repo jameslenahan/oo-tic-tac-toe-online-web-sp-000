ccd4740ba4131a3b60f8339a044ddbe9d165dbf3
James Lenahan <james.k.lenahan@gmail.com>
2019-12-10 16:02:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,8 +4,15 @@ class TicTacToe
   end
   WIN_COMBINATIONS = [
     [0,1,2]
-    [3,4,5]
-    []]
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+    ]
+    
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
   end
